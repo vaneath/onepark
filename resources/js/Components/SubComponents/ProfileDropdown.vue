046a467d453f492c774f33a8c090f525e8b15663
vaneath <vaneathzz@gmail.com>
2024-01-15 23:01:37 +0700

    close #9 migrate sub component for default layout

---
+++ resources/js/Components/SubComponents/ProfileDropdown.vue
@@ -0,0 +1,53 @@
+<script setup>
+import Dropdown from "@/Components/Dropdown.vue";
+import DropdownLink from "@/Components/DropdownLink.vue";
+</script>
+
+<template>
+    <div class="sm:flex sm:items-center sm:ms-6">
+        <!-- Settings Dropdown -->
+        <div class="ms-3 relative">
+            <Dropdown width="48">
+                <template #trigger>
+                    <span class="inline-flex rounded-md">
+                        <button
+                            type="button"
+                            class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none transition ease-in-out duration-150"
+                        >
+                            {{
+                                $page.props.auth.user.first_name +
+                                " " +
+                                $page.props.auth.user.last_name
+                            }}
+                            <svg
+                                class="ms-2 -me-0.5 h-4 w-4"
+                                xmlns="http://www.w3.org/2000/svg"
+                                viewBox="0 0 20 20"
+                                fill="currentColor"
+                            >
+                                <path
+                                    fill-rule="evenodd"
+                                    d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                                    clip-rule="evenodd"
+                                />
+                            </svg>
+                        </button>
+                    </span>
+                </template>
+
+                <template #content>
+                    <DropdownLink :href="route('profile.edit')">
+                        Profile
+                    </DropdownLink>
+                    <DropdownLink
+                        :href="route('logout')"
+                        method="post"
+                        as="button"
+                    >
+                        Log Out
+                    </DropdownLink>
+                </template>
+            </Dropdown>
+        </div>
+    </div>
+</template>
