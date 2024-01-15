9eebf8c7bfa37b16c4b5cb7346b36334230aed84
vaneath <vaneathzz@gmail.com>
2024-01-16 01:10:03 +0700

    close #12 add calendar and modify layout

---
+++ resources/js/Components/AppNavigation.vue
@@ -0,0 +1,34 @@
+<script setup>
+import ApplicationLogo from "@/Components/ApplicationLogo.vue";
+import NavigationToggle from "@/Components/SubComponents/NavigationToggle.vue";
+import ProfileDropdown from "@/Components/SubComponents/ProfileDropdown.vue";
+import { Link } from "@inertiajs/vue3";
+</script>
+
+<template>
+    <nav class="bg-gray-800">
+        <!-- Primary Navigation Menu -->
+        <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+            <div class="flex justify-between h-16">
+                <div class="flex">
+                    <!-- Logo -->
+                    <div class="shrink-0 flex items-center">
+                        <Link :href="route('dashboard')">
+                            <ApplicationLogo
+                                class="block h-9 w-auto fill-current text-white"
+                            />
+                        </Link>
+                    </div>
+                </div>
+
+                <div class="hidden sm:inline-flex">
+                    <ProfileDropdown />
+                </div>
+
+                <div class="sm:hidden inline-flex">
+                    <NavigationToggle />
+                </div>
+            </div>
+        </div>
+    </nav>
+</template>
---
+++ resources/js/Components/SideBar.vue
@@ -0,0 +1,26 @@
+<script setup>
+import SideBarLink from "@/Components/SubComponents/SideBarLink.vue";
+</script>
+
+<template>
+    <aside
+        class="fixed transition-transform -translate-x-full sm:translate-x-0"
+    >
+        <div class="px-3 py-4 overflow-y-auto bg-gray-50 dark:bg-gray-800">
+            <ul class="space-y-2">
+                <SideBarLink
+                    :active="route().current('dashboard')"
+                    :href="route('dashboard')"
+                    icon="fa-database"
+                    name="Dashboard"
+                />
+                <SideBarLink
+                    :active="route().current('login')"
+                    :href="route('dashboard')"
+                    icon="fa-database"
+                    name="Dashboard"
+                />
+            </ul>
+        </div>
+    </aside>
+</template>
