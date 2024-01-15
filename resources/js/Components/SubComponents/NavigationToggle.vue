a210189a7f080469b4e9442c380334b7439aca83
vaneath <vaneathzz@gmail.com>
2024-01-15 23:01:37 +0700

    close #8 migrate sub component for default layout

---
+++ resources/js/Components/SubComponents/NavigationToggle.vue
@@ -0,0 +1,98 @@
+<script setup>
+import { ref } from "vue";
+import ApplicationLogo from "@/Components/ApplicationLogo.vue";
+import Dropdown from "@/Components/Dropdown.vue";
+import DropdownLink from "@/Components/DropdownLink.vue";
+import NavLink from "@/Components/NavLink.vue";
+import ResponsiveNavLink from "@/Components/ResponsiveNavLink.vue";
+import { Link } from "@inertiajs/vue3";
+
+const showingNavigationDropdown = ref(false);
+</script>
+
+<template>
+    <!-- Hamburger -->
+    <div class="inline-flex">
+        <div class="flex items-center">
+            <button
+                @click="showingNavigationDropdown = !showingNavigationDropdown"
+                class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 transition duration-150 ease-in-out"
+            >
+                <svg
+                    class="h-6 w-6"
+                    stroke="currentColor"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                >
+                    <path
+                        :class="{
+                            hidden: showingNavigationDropdown,
+                            'inline-flex': !showingNavigationDropdown,
+                        }"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        stroke-width="2"
+                        d="M4 6h16M4 12h16M4 18h16"
+                    />
+                    <path
+                        :class="{
+                            hidden: !showingNavigationDropdown,
+                            'inline-flex': showingNavigationDropdown,
+                        }"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        stroke-width="2"
+                        d="M6 18L18 6M6 6l12 12"
+                    />
+                </svg>
+            </button>
+        </div>
+        <!-- Responsive Navigation Menu -->
+        <div
+            :class="{
+                hidden: !showingNavigationDropdown,
+            }"
+            class="fixed top-16 right-3 w-56"
+        >
+            <div class="bg-gray-700">
+                <div class="pt-2 pb-3 space-y-1">
+                    <ResponsiveNavLink
+                        :href="route('dashboard')"
+                        :active="route().current('dashboard')"
+                    >
+                        Dashboard
+                    </ResponsiveNavLink>
+                </div>
+
+                <!-- Responsive Settings Options -->
+                <div class="pt-4 pb-1 border-t border-gray-200">
+                    <div class="px-4">
+                        <div class="font-medium text-base text-white">
+                            {{
+                                $page.props.auth.user.first_name +
+                                " " +
+                                $page.props.auth.user.last_name
+                            }}
+                        </div>
+                        <div class="font-medium text-sm text-gray-500">
+                            {{ $page.props.auth.user.email }}
+                        </div>
+                    </div>
+
+                    <div class="mt-3 space-y-1">
+                        <ResponsiveNavLink :href="route('profile.edit')">
+                            Profile
+                        </ResponsiveNavLink>
+                        <ResponsiveNavLink
+                            :href="route('logout')"
+                            method="post"
+                            as="button"
+                        >
+                            Log Out
+                        </ResponsiveNavLink>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
