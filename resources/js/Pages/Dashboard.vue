f21c8a0f961f8ca8b3b83ed042cecdfb3f1d384a
vaneath <vaneathzz@gmail.com>
2024-01-15 20:48:25 +0700

    close #7 add sidebar to homepage

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -1,22 +1,13 @@
 <script setup>
+import Aside from "@/Components/Aside.vue";
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout.vue";
 import { Head } from "@inertiajs/vue3";
 </script>
 
 <template>
-  <Head title="Dashboard" />
+    <Head title="Dashboard" />
 
-  <AuthenticatedLayout>
-    <template #header>
-      <h2 class="font-semibold text-xl text-gray-800 leading-tight">Dashboard</h2>
-    </template>
-
-    <div class="py-12">
-      <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-        <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg p-4">
-          You're logged in!
-        </div>
-      </div>
-    </div>
-  </AuthenticatedLayout>
+    <AuthenticatedLayout>
+        <Aside />
+    </AuthenticatedLayout>
 </template>
