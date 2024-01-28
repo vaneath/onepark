4c81db6cb7fd92c17e9193920d531c3fba1b6a0f
vaneath <vaneathzz@gmail.com>
2024-01-28 16:08:10 +0700

    close #14 add border under navigation

--- resources/js/Components/AppNavigation.vue
+++ resources/js/Components/AppNavigation.vue
@@ -6,7 +6,7 @@ import { Link } from "@inertiajs/vue3";
 </script>
 
 <template>
-    <nav class="bg-gray-800">
+    <nav class="bg-gray-800 border-b-2 border-yellow-500">
         <!-- Primary Navigation Menu -->
         <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
             <div class="flex justify-between h-16">
